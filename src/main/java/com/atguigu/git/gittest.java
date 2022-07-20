a4986259267fbf7dcdf690095bb37de2c2b524bd
hez <hez@qq.com>
2022-07-21 00:12:36 +0800

    hot-fix commit

--- src/main/java/com/atguigu/git/gittest.java
+++ src/main/java/com/atguigu/git/gittest.java
@@ -4,5 +4,6 @@ public class gittest {
     public static void main(String[] args){
         System.out.println("hello11 gittttt2");
         System.out.println("hello11 gittttt3");
+        System.out.println("hello11 gittttt3");
     }
 }
