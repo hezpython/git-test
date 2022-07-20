0afb13d37967385c8acbb35d4623f934ef8e436e
hez <hez@qq.com>
2022-07-21 00:04:28 +0800

    second commit

--- src/main/java/com/atguigu/git/gittest.java
+++ src/main/java/com/atguigu/git/gittest.java
@@ -2,6 +2,6 @@ package com.atguigu.git;
 
 public class gittest {
     public static void main(String[] args){
-        System.out.println("hello11 gittttt");
+        System.out.println("hello11 gittttt2");
     }
 }
