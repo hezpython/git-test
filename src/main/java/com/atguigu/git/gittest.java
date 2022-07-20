8968059d5a1cbbc412e68e72ab0ea847afc86a22
hezpython <109618335+hezpython@users.noreply.github.com>
2022-07-21 01:12:16 +0800

    pull commit

--- src/main/java/com/atguigu/git/gittest.java
+++ src/main/java/com/atguigu/git/gittest.java
@@ -7,5 +7,6 @@ public class gittest {
         System.out.println("hello11 gittttt4");
         System.out.println("master test");
         System.out.println("hello11 gittttt5");
+         System.out.println("pull test");
     }
 }
